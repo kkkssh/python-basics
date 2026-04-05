eee7092418123c7814654594e70f588ac9b9044b
kkkssh <sanghee660@gmail.com>
2026-04-05 17:42:23 +0100

    refactor: improve readability in variables.py

--- variables.py
+++ variables.py
@@ -1,18 +1,24 @@
-# Variable : programs use variables to remember information
-#           if we want a variable name with multiple words, use snake case
+# =========================
+# VARIABLES
+# =========================
+
+# VARIABLES: store information
+# use snake_case for multiple words
 
 # = : we use = sign to store a value inside it
 
 # Lines of code are instructions for the computer to follow
 
-# print() : we tell the computer to display a value in an area called te console
+# print() : we tell the computer to display a value in an area called the console
 
+
+# BASIC VARIABLES
 name = "Chloe"
 print(name)
 print("Hello, World!")
 
 
-# Updating variables : the values they store can change
+# UPDATING VARIABLES : the values they store can change
 status = "Relaxing"
 status = "Watching Netflix"
 print(status)
@@ -21,8 +27,7 @@ status = "Reading a book"
 print(status)
 
 
-# We can also give variables the values of other variables
-
+# ASSIGNING FROM OTHER VARIABLES
 default_option = "upload"
 new_status = "download"
 
