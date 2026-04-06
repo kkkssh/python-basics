23663ec66f42f7f0ec3db06d860635ffddbbd0a9
kkkssh <sanghee660@gmail.com>
2026-04-06 16:45:16 +0100

    feat: add if_statements.py with basic examples

---
+++ if_statements.py
@@ -0,0 +1,27 @@
+# =========================
+# IF STATEMENTS
+# =========================
+
+# IF STATEMENT : run code only if it is evaluated as true
+# CONDITIONS : True, False - decide if the code runs or gets skipped 
+# CONDITIONALS : statements relying on conditions
+if True:
+    print("Hello!")
+
+# if we want to skip the code, use False
+if False:
+    print("Hi!")
+
+# INDENTATION : the space between the code and the code editor's margin
+# INDENTATION OF 4 SPACES : to highlight code blocks, like indenting this display statement
+# CODE BLOCKS : codes that are indented after an if statement. It can be more than a one-liner
+con = True
+if con:
+    print("Look at me")
+    print("I'm a code block")
+
+# IndentationError : if the indentation is incorrect
+if con:
+    print("Look at me")
+#  print("I'm a code block")
+
