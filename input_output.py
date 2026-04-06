d34cef9c816ed3d8a12088667a51cff55a62cc09
kkkssh <sanghee660@gmail.com>
2026-04-06 16:10:34 +0100

    feat: add input_output.py with basic examples

---
+++ input_output.py
@@ -0,0 +1,20 @@
+# =========================
+# INPUT AND OUTPUT
+# =========================
+
+# BASIC OUTPUT
+print("This program can ask questions and display answers.")
+
+# BASIC INPUT
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}!")
+
+# INPUT WITH NUMBERS
+age = int(input("How old are you? "))
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+
+# ANOTHER STRING INPUT
+color = input("What's your favorite color? ")
+print(f"Oh, {color} is a beautiful color!")
