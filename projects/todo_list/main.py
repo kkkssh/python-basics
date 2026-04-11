63dea22386bd5f122d9e70439854b70341dfa5f7
kkkssh <sanghee660@gmail.com>
2026-04-11 15:08:53 +0100

    feat: add basic todo list CLI application

---
+++ projects/todo_list/main.py
@@ -0,0 +1,45 @@
+# MINI PROJECT 2 - TODO LIST
+
+# Description:
+# A simple command-line TODO list program.
+
+# Requirements:
+# - User can add tasks
+# - User can view tasks
+# - User can remove tasks
+# - Program runs until user exits
+
+# Features:
+# - Allows user to add tasks to the list
+# - Displays all tasks currently stored
+# - Allows user to remove tasks from the list
+# - Repeats continuously until the user chooses to exit
+
+
+
+todo_lists = []
+
+while True:
+
+    print("1. Add a list")
+    print("2. View todo lists")
+    print("3. Remove a list")
+
+    choice = input("Enter the number of the menu(1/2/3): ")
+
+    if choice == "1":
+        todo = input("Write your todo list: ")
+        todo_lists.append(todo)
+    
+    elif choice == "2":
+        print(todo_lists)
+
+    elif choice == "3":
+        delete = input("Remove your task: ")
+        todo_lists.remove(delete)
+    
+    else:
+        print("Choose the right number")
+        break
+
+
