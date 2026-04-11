98301b337b755b82fe8c206d060da52ca75a7991
kkkssh <sanghee660@gmail.com>
2026-04-11 15:32:55 +0100

    feat: add numbered display and empty list handling

--- projects/todo_list/main.py
+++ projects/todo_list/main.py
@@ -11,7 +11,8 @@
 
 # Features:
 # - Allows user to add tasks to the list
-# - Displays all tasks currently stored
+# - Displays all tasks with numbering
+# - Shows a message if the list is empty
 # - Allows user to remove tasks from the list
 # - Repeats continuously until the user chooses to exit
 
@@ -21,9 +22,9 @@ todo_lists = []
 
 while True:
 
-    print("1. Add a list")
+    print("1. Add a task")
     print("2. View todo lists")
-    print("3. Remove a list")
+    print("3. Remove a task")
     print("4. Exit")
 
     choice = input("Enter the number of the menu(1/2/3/4): ")
@@ -31,9 +32,16 @@ while True:
     if choice == "1":
         todo = input("Write your todo list: ")
         todo_lists.append(todo)
+        print("Task added")
     
     elif choice == "2":
-        print(todo_lists)
+        if len(todo_lists) == 0:
+            print("Your todo list is empty")
+        else:
+            index = 1
+            for task in todo_lists:
+                print(f"{index}. {task}")
+                index += 1
 
     elif choice == "3":
         delete = input("Remove your task: ")
