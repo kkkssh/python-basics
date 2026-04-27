82c5cbb72f30f8ef5ade5d328cde4eb3e8a81e2a
kkkssh <sanghee660@gmail.com>
2026-04-27 21:38:28 +0100

    feat: add split with maxsplit examples to strings.py

--- fundamentals/strings.py
+++ fundamentals/strings.py
@@ -7,10 +7,22 @@ fruit_text = "apple banana grape"
 fruit_list = fruit_text.split()
 print(fruit_list)    # ['apple', 'banana', 'grape']
 
+
 # SPLIT WITH SEPARATOR
 fruit_text_with_commas = "apple,banana,grape"
 fruit_list_from_commas = fruit_text_with_commas.split(",")
 print(fruit_list_from_commas)    # ['apple', 'banana', 'grape']
+
+
+# SPLIT WITH MAX SPLITS
+domain = "mail.google.com"
+
+full_split = domain.split(".")
+print(full_split)  # ['mail', 'google', 'com']
+
+limited_split = domain.split(".", 1)
+print(limited_split)  # ['mail', 'google.com']
+
 
 # REPLACE (STRING UPDATE)
 sentence = "Today's special is pasta"
