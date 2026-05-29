9db408a37d17df3311009f26e637b850648aaf6c
kkkssh <sanghee660@gmail.com>
2026-05-29 12:38:32 +0100

    feat: add module examples using math module

---
+++ fundamentals/modules.py
@@ -0,0 +1,18 @@
+# =========================
+# MODULES
+# =========================
+
+# a module is a Python file
+# that contains code such as
+# functions, variables, or classes
+
+# modules help organize code
+# and make it reusable
+
+import math
+
+print(f"The value of pi is {math.pi}")
+print(f"The square root of 25 is {math.sqrt(25)}")
+print(f"Rounded up to the nearest number {math.ceil(22.7324)}")
+
+# help(math)  # displays documentation for the module
