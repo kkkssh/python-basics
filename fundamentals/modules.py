7adca19f76a8a4556c4b53c415bc2d6230b16ae6
kkkssh <sanghee660@gmail.com>
2026-05-30 12:20:32 +0100

    feat: add module import examples

--- fundamentals/modules.py
+++ fundamentals/modules.py
@@ -9,6 +9,9 @@
 # modules help organize code
 # and make it reusable
 
+
+# IMPORTING A MODULE
+
 import math
 
 print(f"The value of pi is {math.pi}")
@@ -16,3 +19,39 @@ print(f"The square root of 25 is {math.sqrt(25)}")
 print(f"Rounded up to the nearest number {math.ceil(22.7324)}")
 
 # help(math)  # displays documentation for the module
+
+
+# IMPORTING ANOTHER MODULE
+
+import statistics
+
+scores = [4, 4, 3, 6, 1, 2, 8, 4]
+mean_score = statistics.mean(scores)
+
+print(f"Mean score is {mean_score}")
+
+
+# IMPORTING MULTIPLE MODULES
+
+import statistics, math
+
+diameters = [9, 7, 4, 6]
+mean_diameter = statistics.mean(diameters)
+
+print(f"Mean diameter is {mean_diameter}")
+print(f"Value of pi is {math.pi}")
+
+
+# IMPORTING SPECIFIC ITEMS
+
+from math import pi
+
+print(f"Value of pi is {pi}")
+
+
+from statistics import mean
+
+test_scores = [33, 7, 4, 6]
+mean_result = mean(test_scores)
+
+print(f"Mean result is {mean_result}")
