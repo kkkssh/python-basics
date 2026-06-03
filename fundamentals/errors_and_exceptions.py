d556a452c464f372cc21fcc26efa181acd5db2fe
kkkssh <sanghee660@gmail.com>
2026-06-03 21:34:48 +0100

    feat: add error and exception notes

---
+++ fundamentals/errors_and_exceptions.py
@@ -0,0 +1,39 @@
+# =========================
+# ERRORS AND EXCEPTIONS
+# =========================
+
+# errors stop a program from running
+
+# exceptions are errors that occur
+# while the program is running
+
+
+# SYNTAX ERRORS
+
+# SyntaxError usually occurs when Python
+# cannot understand the code
+
+# misspelled keywords
+# iff cost > 10:
+
+# missing symbols
+# if cost < 10
+#     print("You can buy it")
+
+# incomplete statements
+# if cost > 10:
+
+
+# INDENTATION ERRORS
+
+# IndentationError is a specific type
+# of SyntaxError
+
+# if cost > 10:
+# print("Too expensive")
+
+
+# CARET (^)
+
+# a caret (^) indicates where Python
+# found the error in the code
