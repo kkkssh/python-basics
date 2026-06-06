5bb481159e18cca4b79d2372439ca9786b8af052
kkkssh <sanghee660@gmail.com>
2026-06-06 13:22:17 +0100

    feat: add common exception examples

--- fundamentals/errors_and_exceptions.py
+++ fundamentals/errors_and_exceptions.py
@@ -37,3 +37,58 @@
 
 # a caret (^) indicates where Python
 # found the error in the code
+
+
+# EXCEPTIONS
+
+# Traceback helps us debug our code,
+# which means finding errors
+# best read from bottom to top
+
+
+# ZeroDivisionError occurs when
+# dividing by zero
+
+# print(10 / 0)
+
+
+# NameError occurs when referencing
+# a variable that does not exist
+
+# print(username)
+
+
+# TypeError occurs when an operation
+# is used with an inappropriate type
+
+# print("5" + 5)
+
+
+# ModuleNotFoundError occurs when Python
+# cannot find a module
+
+# import tiime
+
+
+# IndexError occurs when an index
+# is out of range
+
+# scores = [25, 50, 10]
+# print(scores[5])
+
+
+# LOGIC ERRORS
+
+# logic errors occur when there is
+# no error or exception, but the code
+# does not produce the expected result
+
+# john = 24
+# alana = 18
+
+# average_age = john + alana / 2
+
+# print(average_age)
+
+# Correct version:
+# average_age = (john + alana) / 2
